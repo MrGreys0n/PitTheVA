5112031413daec9871139e2deeb70bf88c461742
ev1nnn <45720190+ev1nnn@users.noreply.github.com>
2020-01-21 20:05:52 +0300

    Update micro.py

--- micro.py
+++ micro.py
@@ -4,11 +4,14 @@ def mindex():
     for index, name in enumerate(sr.Microphone.list_microphone_names()):
         if index > 5:
             break
-        if index % 2 == 1:
-            print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+        print("Микрофон с именем \"{1}\" найден с (device_index={0})".format(index, name))
     index = input("Введите device_index своего микрофона одной цифрой: ")
-    if index in ('1', '3', '5'):
-        return(int(index))
+    if index in ('0', '1', '2', '3', '4', '5'):
+        with open('index.txt', 'w') as f:
+            f.write(index)
+            print('ok')
     else:
         print('Вы ввели некорректный device_index!')
         mindex()
+
+mindex()
