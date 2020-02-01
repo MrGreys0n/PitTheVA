444d79b18e676ce5080593805dad09a3638fb6f5
ev1nnn <45720190+ev1nnn@users.noreply.github.com>
2020-02-01 21:28:40 +0300

    Update micro.py
    -Отсеивание динамиков и прочих устройств, не сильно похожих на микрофоны
    -Отсеивание повторяющихся устройств (при вызове не первого, система не слышит пользователя, но это не вызывает ошибки в основной программе
    -Незначительное изменение выводимого текста и дополнительных проверок

--- micro.py
+++ micro.py
@@ -1,17 +1,37 @@
 import speech_recognition as sr
 
+
+NOT_MICRO = ["переназначение", "первичный", "драйвер", "динамики", "nvidia"]
+MICRO = True
+indexes = []
+names = []
+
 def mindex():
     for index, name in enumerate(sr.Microphone.list_microphone_names()):
-        if index > 5:
+        if index > 10:
             break
-        print("Микрофон с именем \"{1}\" найден с (device_index={0})".format(index, name))
-    index = input("Введите device_index своего микрофона одной цифрой: ")
-    if index in ('0', '1', '2', '3', '4', '5'):
+
+        for word in NOT_MICRO:
+            if word in name.lower():
+                MICRO = False
+
+        if MICRO:
+            if name not in names:
+                print("Микрофон \"{1}\" найден с (device_index={0})".format(index, name))
+                indexes.append(str(index))
+                names.append(name)
+        else:
+            MICRO = True
+
+    index = input("Введите device_index своего микрофона: ")
+
+    if index in indexes:
         with open('index.txt', 'w') as f:
             f.write(index)
             print('ok')
     else:
         print('Вы ввели некорректный device_index!')
+        print('----------------------------------------------------------------------------')
         mindex()
 
 mindex()
